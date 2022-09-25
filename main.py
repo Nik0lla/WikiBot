e1d81e06580b4a2f0c849d53204e5e8d948a3fcb
Nikolay Gorozhankin <softnick@yandex.ru>
2022-09-25 19:32:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+import wikipedia
+
+
+bot = telebot.TeleBot('5193102030:AAEPkcGnPTxT4RjzvSdmkri9C5ZgpjpVFV4')
+wikipedia.set_lang('ru')
+
+def getwiki(messge):
+    try:
+        page = wikipedia.page(messge)
+        wikitext = page.content[:2000]
+
+        return wikitext
+    except Exception as e:
+        return 'В базе нет информации об этом'
+
+# Функция - обработчика команды /start
+@bot.message_handler(commands=['start'])
+def start(messge, res=False):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = telebot.types.KeyboardButton('Yandex')
+    markup.add(button)
+    bot.send_message(messge.chat.id, 'Я на сязи!', reply_markup=markup)
+    print(messge)
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+bot.polling(none_stop=True, interval=10)
